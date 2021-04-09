68aac759a1f3992f433c3971421d96feca3b3e4c
staridorde <starcevicmilos2@gmail.com>
2021-04-09 20:17:35 +0200

    initial push

---
+++ resources/views/profiles/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="/img/bioshock.png" alt="" class="rounded-circle">
+        </div>
+
+        <div class="col-9">
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user->username}}</h1>
+                <a href="/p/create">Add New Post</a>
+            </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
+            </div>
+            <div class="pt-5 font-weight-bold"> {{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description}}</div>
+            <div><a href="#">{{ $user->profile->url}}</a></div>
+        </div>
+    </div>
+
+    <div class="row">
+        @foreach($user->posts as $post)
+        
+
+        <div class="col-4 p-4">
+            <img src="/storage/{{$post->image }}" alt="" class="w-100">
+        </div>
+
+        @endforeach
+    </div>
+</div>
+@endsection
